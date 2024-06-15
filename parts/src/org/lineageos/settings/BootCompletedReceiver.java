PixelExperience-Lisa/device_xiaomi_sm8350-common

sm8350-common: parts: Override HDR types for dolby vision
Stock display HAL doesn't enable dolby vision support,
instead miui uses a custom surfaceflinger lib for the
same. Since we won't be using that, simply override
supported HDR types at boot.

parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -20,6 +20,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.IBinder;
+import android.view.Display.HdrCapabilities;
+import android.view.SurfaceControl;
 
 import org.lineageos.settings.dirac.DiracUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
@@ -33,5 +36,11 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
         // Thermal Profiles
         ThermalUtils.startService(context);
+
+        // Override HDR types
+        final IBinder displayToken = SurfaceControl.getInternalDisplayToken();
+        SurfaceControl.overrideHdrTypes(displayToken, new int[]{
+                HdrCapabilities.HDR_TYPE_DOLBY_VISION, HdrCapabilities.HDR_TYPE_HDR10,
+                HdrCapabilities.HDR_TYPE_HLG, HdrCapabilities.HDR_TYPE_HDR10_PLUS});
     }
 }

vili-develop/android_device_xiaomi_sm8350-common

parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -11,6 +11,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.util.Log;
+import android.view.SurfaceControl;
+import android.content.SharedPreferences;
+import android.os.SystemProperties;
+import android.view.Display.HdrCapabilities;
 
 import org.lineageos.settings.refreshrate.RefreshUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
@@ -32,5 +36,11 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
         // Thermal
         ThermalUtils.startService(context);
+
+        // Override HDR types
+        final IBinder displayToken = SurfaceControl.getInternalDisplayToken();
+        SurfaceControl.overrideHdrTypes(displayToken, new int[]{
+                HdrCapabilities.HDR_TYPE_DOLBY_VISION, HdrCapabilities.HDR_TYPE_HDR10,
+                HdrCapabilities.HDR_TYPE_HLG, HdrCapabilities.HDR_TYPE_HDR10_PLUS});
     }
 }